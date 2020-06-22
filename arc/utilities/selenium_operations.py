0c9fc9613c17be203fc5079d18b5ee67b84d99c9
Jason Deters <Jason.Deters@usace.army.mil>
2020-06-21 21:57:56 -0700

    ignore win32api error

--- arc/utilities/selenium_operations.py
+++ arc/utilities/selenium_operations.py
@@ -34,7 +34,10 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.common.by import By
-from win32api import GetFileVersionInfo, LOWORD, HIWORD
+try:
+    from win32api import GetFileVersionInfo, LOWORD, HIWORD
+except:
+    pass
 
 # Import Custom Libraries
 try:
