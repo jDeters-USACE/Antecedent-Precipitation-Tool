d9c0bb69c75ff7af8e51093ead33a6e3b95fc4b8
Gutenson <1517780819121002@mil>
2021-05-03 09:06:05 -0500

    push consitutes all changes made to create v1.0.19.

--- arc/shortcut.py
+++ arc/shortcut.py
@@ -1,15 +1,15 @@
 #  This software was developed by United States Army Corps of Engineers (USACE)
 #  employees in the course of their official duties.  USACE used copyrighted,
-#  open source code to develop this software, as such this software 
+#  open source code to develop this software, as such this software
 #  (per 17 USC § 101) is considered "joint work."  Pursuant to 17 USC § 105,
 #  portions of the software developed by USACE employees in the course of their
 #  official duties are not subject to copyright protection and are in the public
 #  domain.
-#  
+#
 #  USACE assumes no responsibility whatsoever for the use of this software by
 #  other parties, and makes no guarantees, expressed or implied, about its
-#  quality, reliability, or any other characteristic. 
-#  
+#  quality, reliability, or any other characteristic.
+#
 #  The software is provided "as is," without warranty of any kind, express or
 #  implied, including but not limited to the warranties of merchantability,
 #  fitness for a particular purpose, and noninfringement.  In no event shall the
@@ -17,12 +17,12 @@
 #  liability, whether in an action of contract, tort or otherwise, arising from,
 #  out of or in connection with the software or the use or other dealings in the
 #  software.
-#  
+#
 #  Public domain portions of this software can be redistributed and/or modified
 #  freely, provided that any derivative works bear some notice that they are
 #  derived from it, and any modified versions bear some notice that they have
-#  been modified. 
-#  
+#  been modified.
+#
 #  Copyrighted portions of the software are annotated within the source code.
 #  Open Source Licenses, included in the source code, apply to the applicable
 #  copyrighted portions.  Copyrighted portions of the software are not in the
@@ -78,7 +78,7 @@ def create_shortcut_frozen():
     # Define Shortcut Variables
     module_path = os.path.dirname(os.path.realpath(__file__))
     root_folder = os.path.split(module_path)[0]
-    frozen_exe_path = '{}\\Antecedent Precipitation Tool.exe'.format(root_folder)
+    frozen_exe_path = '{}\\main_ex.exe'.format(root_folder)
     desktop_path = str(winshell.desktop()) # get desktop path
     icon = "{}\\images\\Graph.ico".format(root_folder)
     shortcut_path = os.path.join(desktop_path, 'Antecedent Precipitation Tool.lnk')
@@ -106,4 +106,3 @@ def create_shortcut_frozen():
 
 if __name__ == '__main__':
     create_shortcut_frozen()
-    
